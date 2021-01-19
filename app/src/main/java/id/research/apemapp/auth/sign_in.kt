bb420f0251a2e91e82ce5241af09dcc63cb6bf65
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-01-19 20:02:49 +0700

    implementasi desain form sign in dan sign up

--- app/src/main/java/id/research/apemapp/auth/sign_in.kt
+++ app/src/main/java/id/research/apemapp/auth/sign_in.kt
@@ -1,12 +1,20 @@
 package id.research.apemapp.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.research.apemapp.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        tv_masuk.setOnClickListener {
+            val daftar = Intent(this@sign_in, sign_up::class.java)
+            startActivity(daftar)
+            finish()
+        }
     }
 }
--- app/src/main/java/id/research/apemapp/auth/sign_up.kt
+++ app/src/main/java/id/research/apemapp/auth/sign_up.kt
@@ -1,12 +1,20 @@
 package id.research.apemapp.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.research.apemapp.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        tv_masuk.setOnClickListener {
+            val masuk = Intent(this@sign_up, sign_in::class.java)
+            startActivity(masuk)
+            finish()
+        }
     }
 }
