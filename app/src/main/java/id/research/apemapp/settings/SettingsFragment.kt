f5c996927540eab1cab0e332fa352e53bb7ce73d
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-07-23 01:06:31 +0700

    add alert dialog in exit profile fragment

--- app/src/main/java/id/research/apemapp/settings/SettingsFragment.kt
+++ app/src/main/java/id/research/apemapp/settings/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package id.research.apemapp.settings
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dev.shreyaspatil.MaterialDialog.MaterialDialog
+import id.research.apemapp.R
 import id.research.apemapp.auth.SignInActivity
 import id.research.apemapp.databinding.FragmentSettingsBinding
 import id.research.apemapp.utils.Constants
@@ -34,23 +37,42 @@ class SettingsFragment : Fragment() {
         myPreferences = MySharedPreferences(this@SettingsFragment.requireContext())
         settingsBinding.tvName.text = myPreferences.getValue(Constants.STUDENT_NAME)
 
+
+
         layout_logout.setOnClickListener {
-            //Menyimpan data bahwa siswa telah berhasil masuk
-            myPreferences.setValue(Constants.STUDENT, "")
 
-            //menyimpan data siswa yang sudah masuk
-            myPreferences.setValue(Constants.STUDENT_ID, " ")
-            myPreferences.setValue(Constants.STUDENT_NAME, " ")
-            myPreferences.setValue(Constants.STUDENT_NIS, " ")
-            myPreferences.setValue(Constants.STUDENT_PASSWORD, " ")
-            myPreferences.setValue(Constants.STUDENT_LOOPING_QUIZ_SCORE, " ")
-            myPreferences.setValue(Constants.STUDENT_ARRAY_QUIZ_SCORE, " ")
-            myPreferences.setValue(Constants.STUDENT_FUNCTION_QUIZ_SCORE, " ")
+            val mDialog = MaterialDialog.Builder(requireContext() as Activity)
+                .setTitle("Keluar")
+                .setMessage("Apakah Anda Yakin Ingin Keluar ?")
+                .setCancelable(true)
+                .setPositiveButton(
+                    "Tidak", R.drawable.ic_cancel
+                ){dialogInterface, which ->
+                    dialogInterface.dismiss()
+                }
+                .setNegativeButton(
+                    "Ya", R.drawable.ic_exit
+                ){
+                    dialogInterface, _ ->
+                    //Menyimpan data bahwa siswa telah berhasil masuk
+                    myPreferences.setValue(Constants.STUDENT, "")
 
-            val goOut = Intent(this@SettingsFragment.context, SignInActivity::class.java)
-            startActivity(goOut)
-            getActivity()?.finish()
+                    //menyimpan data siswa yang sudah masuk
+                    myPreferences.setValue(Constants.STUDENT_ID, " ")
+                    myPreferences.setValue(Constants.STUDENT_NAME, " ")
+                    myPreferences.setValue(Constants.STUDENT_NIS, " ")
+                    myPreferences.setValue(Constants.STUDENT_PASSWORD, " ")
+                    myPreferences.setValue(Constants.STUDENT_LOOPING_QUIZ_SCORE, " ")
+                    myPreferences.setValue(Constants.STUDENT_ARRAY_QUIZ_SCORE, " ")
+                    myPreferences.setValue(Constants.STUDENT_FUNCTION_QUIZ_SCORE, " ")
 
+                    val goOut = Intent(this@SettingsFragment.context, SignInActivity::class.java)
+                    startActivity(goOut)
+                    getActivity()?.finish()
+                }
+                .build()
+            //show dialog
+            mDialog.show()
         }
     }
 }
