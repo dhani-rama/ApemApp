1b2041e94c35ef21e2fa1e6f2b5b8209fc51c1ff
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-07-24 17:35:52 +0700

    replace layout my account

--- app/src/main/java/id/research/apemapp/settings/SettingsFragment.kt
+++ app/src/main/java/id/research/apemapp/settings/SettingsFragment.kt
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import dev.shreyaspatil.MaterialDialog.MaterialDialog
+import es.dmoral.toasty.Toasty
 import id.research.apemapp.R
 import id.research.apemapp.auth.SignInActivity
 import id.research.apemapp.databinding.FragmentSettingsBinding
+import id.research.apemapp.settings.Alarm.AlarmActivity
 import id.research.apemapp.utils.Constants
 import id.research.apemapp.utils.MySharedPreferences
 import kotlinx.android.synthetic.main.fragment_settings.*
@@ -35,11 +38,32 @@ class SettingsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         myPreferences = MySharedPreferences(this@SettingsFragment.requireContext())
-        settingsBinding.tvName.text = myPreferences.getValue(Constants.STUDENT_NAME)
+//        settingsBinding.tvName.text = myPreferences.getValue(Constants.STUDENT_NAME)
+
+        settingsBinding.btnAlarm.setOnClickListener {
+            startActivity(Intent(this.requireActivity(), AlarmActivity::class.java))
+//            activity?.finish()
+        }
+
+        settingsBinding.btnEditProfile.setOnClickListener {
+            Toasty.info(this.requireActivity(), "Belum Aku Coding Sayang :)", Toast.LENGTH_LONG).show()
+        }
+
+//        settingsBinding.btnEditPass.setOnClickListener {
+//            Toasty.info(this.requireActivity(), "Belum Aku Coding Sayang :)", Toast.LENGTH_LONG).show()
+//        }
+
+        settingsBinding.btnProfilDeveloper.setOnClickListener {
+            startActivity(Intent(this.requireActivity(), DeveloperProfileActivity::class.java))
+//            Toasty.info(this.requireActivity(), "Belum Aku Coding Sayang :)", Toast.LENGTH_LONG).show()
+        }
+
+        settingsBinding.btnInstructionUsed.setOnClickListener {
+            Toasty.info(this.requireActivity(), "Belum Aku Coding Sayang :)", Toast.LENGTH_LONG).show()
+        }
 
 
-
-        layout_logout.setOnClickListener {
+        settingsBinding.btnLogout.setOnClickListener {
 
             val mDialog = MaterialDialog.Builder(requireContext() as Activity)
                 .setTitle("Keluar")
@@ -59,7 +83,8 @@ class SettingsFragment : Fragment() {
 
                     //menyimpan data siswa yang sudah masuk
                     myPreferences.setValue(Constants.STUDENT_ID, " ")
-                    myPreferences.setValue(Constants.STUDENT_NAME, " ")
+                    myPreferences.setValue(Constants.STUDENT_FIRST_NAME, " ")
+                    myPreferences.setValue(Constants.STUDENT_LAST_NAME, " ")
                     myPreferences.setValue(Constants.STUDENT_NIS, " ")
                     myPreferences.setValue(Constants.STUDENT_PASSWORD, " ")
                     myPreferences.setValue(Constants.STUDENT_LOOPING_QUIZ_SCORE, " ")
