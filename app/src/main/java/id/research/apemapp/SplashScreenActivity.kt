4db1aef2d951aa22edbbfca670b8c64feb5c80d7
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-07-31 07:19:43 +0700

    replace image to animation logo apem in layout splash screen

--- app/src/main/java/id/research/apemapp/SplashScreenActivity.kt
+++ app/src/main/java/id/research/apemapp/SplashScreenActivity.kt
@@ -1,21 +1,24 @@
 package id.research.apemapp.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import id.research.apemapp.R
+import androidx.appcompat.app.AppCompatActivity
 import id.research.apemapp.auth.SignInActivity
-import kotlinx.android.synthetic.main.activity_splash_screen.*
+import id.research.apemapp.databinding.ActivitySplashScreenBinding
 
-class on_boarding1 : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var mSplashBinding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        mSplashBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(mSplashBinding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val lanjut = Intent(this@on_boarding1, SignInActivity::class.java)
+            val lanjut = Intent(this, SignInActivity::class.java)
             startActivity(lanjut)
             finish()
         }, 3000)
