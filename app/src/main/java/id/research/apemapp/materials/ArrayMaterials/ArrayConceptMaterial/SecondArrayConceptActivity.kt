9143ca2567490a6ed791da001933324bbcc4cad0
Ahmad Ramadhani <49054386+dhani-rama@users.noreply.github.com>
2021-08-25 09:21:54 +0700

    replace layout array concept and add image about array concept

--- app/src/main/java/id/research/apemapp/materials/ArrayMaterials/ArrayConceptMaterial/SecondArrayConceptActivity.kt
+++ app/src/main/java/id/research/apemapp/materials/ArrayMaterials/ArrayConceptMaterial/SecondArrayConceptActivity.kt
@@ -2,11 +2,16 @@ package id.research.apemapp.materials.ArrayMaterials.ArrayConceptMaterial
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import es.dmoral.toasty.Toasty
 import id.research.apemapp.databinding.ActivitySecondArrayConceptBinding
 import id.research.apemapp.materials.ArrayMaterials.ArrayDetailMaterialsActivity
+import id.research.apemapp.utils.Constants
 
-class SecondArrayConceptActivity : AppCompatActivity() {
+class SecondArrayConceptActivity : YouTubeBaseActivity() {
 
     private lateinit var mSecondBinding: ActivitySecondArrayConceptBinding
 
@@ -24,6 +29,30 @@ class SecondArrayConceptActivity : AppCompatActivity() {
             startActivity(Intent(this, ArrayDetailMaterialsActivity::class.java))
             finish()
         }
+
+        mSecondBinding.videoFirst.initialize(Constants.API_YT_KEY,
+            object : YouTubePlayer.OnInitializedListener {
+                override fun onInitializationSuccess(
+                    provider: YouTubePlayer.Provider?,
+                    youtubePlayer: YouTubePlayer?,
+                    wasRestored: Boolean
+                ) {
+                    if (wasRestored) {
+                        youtubePlayer!!.play()
+                    } else {
+                        youtubePlayer!!.cueVideo("DwPE9zsX00w")
+                    }
+                }
+
+                override fun onInitializationFailure(
+                    provider: YouTubePlayer.Provider?,
+                    result: YouTubeInitializationResult?
+                ) {
+                    Toasty.info(this@SecondArrayConceptActivity, "$result", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
 
     }
 }
